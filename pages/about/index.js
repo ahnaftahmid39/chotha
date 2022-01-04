43e11b23e4e360f72c3135de5c64d726a9d8e0e8
Ahnaf <tahmidtahmid31@gmail.com>
2022-01-05 02:13:04 +0600

    fix: apos escape

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,25 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-
-      <div>
-        <Head></Head>
-        <div style={{display: "flex", alignItems: 'center', justifyContent: 'center', margin: '10%', fontFamily: 'monospace', fontWeight: '600'}}>
-          Hi, I'm Ahnaf Tahmid, creator of chotha. This is just a hobby project. I wish to see it grow bigger. It is open source. Feel free to contribute.
-
-        </div>
+    <div>
+      <Head></Head>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: '10%',
+          fontFamily: 'monospace',
+          fontWeight: '600',
+        }}
+      >
+        <p>
+          {`Hi, I'm Ahnaf Tahmid, creator of chotha. This is just a hobby project.
+          I wish to see it grow bigger. It is open source. Feel free to
+          contribute.`}
+        </p>
       </div>
+    </div>
   );
 }
