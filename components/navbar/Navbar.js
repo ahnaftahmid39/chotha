245c7b195bdf38b6fed1175189221bc6ba698e9e
Ahnaf <tahmidtahmid31@gmail.com>
2022-01-22 01:45:43 +0600

    fix: navbar component

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './Navbar.module.css';
+const Navbar = ({}) => {
+  const router = useRouter();
+  return (
+    <nav className={styles['navbar-container']}>
+      <div className={styles['links']}>
+        {
+          (router.pathname != '/' && (
+            <Link href={'/'} passHref>
+              <a title={'Home'}>Home</a>
+            </Link>
+          ))
+        }
+        {
+          (router.pathname != '/about' && (
+            <Link href={'/about'} passHref>
+              <a title={'About'}>About</a>
+            </Link>
+          ))
+        }
+        {
+          (router.pathname != '/new-chotha' && (
+            <Link href={'/new-chotha'} passHref>
+              <a title={'New Chotha'}>New Chotha</a>
+            </Link>
+          ))
+        }
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
