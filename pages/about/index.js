576fb1e04f82c20866b0c034475bed548f9e21f0
Ahnaf Tahmid <tahmidtahmdi31@gmail.com>
2023-03-25 18:08:33 +0600

    added github link to about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,9 +9,9 @@ export default function Home() {
       </Head>
       <div className={styles['container']}>
         <p className={styles['about']}>
-          {`Hi, I'm Ahnaf Tahmid, creator of chotha. This is just a hobby project.
-          I wish to see it grow bigger. It is open source. Feel free to
-          contribute.`}
+          Hi. This is just a hobby project. We wish to see it grow bigger. It is
+          open source. Feel free to contribute. Check our github:{'  '}
+          <a href='https://github.com/ahnaftahmid39/chotha'> Chotha </a>
         </p>
       </div>
     </div>
