500fa747d713eca92d71c0a610b2cebe8ce7feec
Ahnaf Tahmid <tahmidtahmid31@gmail.com>
2023-04-20 04:10:45 +0600

    fix: some css error

--- components/cards/post_card/PostCard.js
+++ components/cards/post_card/PostCard.js
@@ -7,23 +7,27 @@ const PostCard = forwardRef(({ post, onClick, href }, ref) => {
   return (
     <div className={`${styles['card']}`}>
       <h2 className={styles['title']}>{title}</h2>
-      <div className={styles['meta']}>
-        <div className={styles['icon']}>
-          <DateIcon />
-        </div>
-        <div className={styles['time']}>
-          {new Date(createdAt).toLocaleDateString('en-UK', {
-            month: 'long',
-            day: 'numeric',
-            year: 'numeric',
-          })}{' '}
-        </div>
-        <div className={styles['icon']}>
-          <PencilIcon />
-        </div>
-        <div className={styles['author']}>{user?.name}</div>
-      </div>
       <div className={styles['description']}>{description}</div>
+      <div className={styles['meta']}>
+        <div className={styles['time-wrapper']}>
+          <div className={styles['icon']}>
+            <DateIcon />
+          </div>
+          <div className={styles['time']}>
+            {new Date(createdAt).toLocaleDateString('en-UK', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })}{' '}
+          </div>
+        </div>
+        <div className={styles['author-wrapper']}>
+          <div className={styles['icon']}>
+            <PencilIcon />
+          </div>
+          <div className={styles['author']}>{user?.name}</div>
+        </div>
+      </div>
       <a
         title={title}
         href={href}
